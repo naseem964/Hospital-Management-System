e2399b31ea70189e9352961b5c242a709f9c47cb
Naseem Jabir <Naseem.wind@gmail.com>
2026-05-08 23:02:26 -0400

    Progress: Implemented manual 'View Patients' logic with ResultSet

--- HospitalManagementSystem/src/main/java/org/example/Main.java
+++ HospitalManagementSystem/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +20,24 @@ public class Main {
             System.out.println("Connecting to database...");
             Connection connection = DriverManager.getConnection(url, user, password);
 
+            //  SQL query to fetch all data
+            String viewSql = "SELECT * FROM patients";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(viewSql);
+
+            System.out.println("\n--- Current Patient List --");
+            System.out.printf("%-5s | %-20s | %-5s | %-20%n", "ID", "Name", "Age", "Ailment");
+            System.out.println("------------------------------------------------------------");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String ailment = resultSet.getString("ailment");
+
+                // Displaying the data in a clean format
+                System.out.printf("%-5d | %-20s | %-5d | %20s%n", id, name, age, ailment);
+            }
 
             String sql = "INSERT INTO patients (name, age, ailment) VALUES (?, ?, ?)";  //  Define the SQL command
 
